0236fe000257970fca992226fc18139491cabcb9
Ilaiyaraja <ilaiyaraja221197@gmail.com>
2024-04-17 15:00:54 +0530

    Fix: product price amount conversion

--- App/Controller/Main.php
+++ App/Controller/Main.php
@@ -12,20 +12,19 @@ class Main
      */
     function renderPrice($price_html, $product_id)
     {
-        /* if (!Woocommerce::is_ajax()) {
-             return $price_html;
-         }*/
         $price = trim(strip_tags($price_html));
-        $replace_strings = array('&#36;', '&nbsp;');
+        //$replace_strings = array('&#36;', '&nbsp;');
         if (!function_exists('wc_get_product')) {
             return $price_html;
         }
         $product = Product::get($product_id);
         $original_price = Product::getPrice($product_id);
-        $prices = explode('&nbsp;', $price);
-        $price = (float)str_replace($replace_strings, '', $prices[0]);
+//        $prices = explode('&nbsp;', $price);
+        $prices = explode(';', $price);
+//        $price = (float)str_replace($replace_strings, '', $prices[0]);
+        $price = apply_filters('wdr_wholesale_compatibility_price', $prices[1], $product_id,$price_html);
         if ($original_price != $price) {
-            $result = apply_filters('wdr_get_product_discounted_price',$price,$product,1,$original_price);
+            $result = apply_filters('wdr_get_product_discounted_price',$price,$product,1,$price);
             if ($result !== false) {
                 $price_html = "<del>{$price_html}</del><ins>" . wc_price($result) . "</ins>";
             }
